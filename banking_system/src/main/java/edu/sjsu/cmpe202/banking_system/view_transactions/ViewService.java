353675505212cae35c6010f5e9206219099e2c4e
kamdarkarnavee <kamdarkarnavee@gmail.com>
2019-12-07 07:17:59 -0800

    Update 18 months transactions views only

--- banking_system/src/main/java/edu/sjsu/cmpe202/banking_system/view_transactions/ViewService.java
+++ banking_system/src/main/java/edu/sjsu/cmpe202/banking_system/view_transactions/ViewService.java
@@ -3,13 +3,14 @@ package edu.sjsu.cmpe202.banking_system.view_transactions;
 import edu.sjsu.cmpe202.banking_system.transactions.Transactions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
 
 @Service
 public class ViewService {
@@ -19,8 +20,30 @@ public class ViewService {
 
     public List<Transactions> viewTransactions(long account_no, String transaction_type, String start_date, String end_date) throws ParseException {
         SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
-        Date date1 = sd.parse(start_date);
-        Date date2 = sd.parse(end_date);
+//        Date date1 = sd.parse(start_date);
+//        Date date2 = sd.parse(end_date);
+
+        Date date1 = null, date2 = null;
+        if(start_date.isEmpty()){
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.MONTH, -1);
+            date1 = cal.getTime();
+        }else{
+            date1 = sd.parse(start_date);
+        }
+
+        if(end_date.isEmpty()){
+            date2 = new Date();
+
+        }else{
+            date2 = sd.parse(end_date);
+        }
+
+        long duration = date2.getTime() - date1.getTime();
+        long diffInDays = TimeUnit.MILLISECONDS.toDays(duration);
+        if(diffInDays > 547.86){
+            throw new ResponseStatusException(NOT_ACCEPTABLE, "Cannot view transactions beyond 18 months");
+        }
         List<Transactions> transactionsList = new ArrayList<>((viewRepository.findByTransaction_dateBetween(date1, date2)));
 
 //        List<Transactions> transactionsList = new ArrayList<>((Collection<? extends Transactions>) viewRepository.findAll());
